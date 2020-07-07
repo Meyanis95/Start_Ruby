b4d97b36ae9b4ccc10f4e24e79be98c4a607dc02
Meyanis95 <b00694485@essec.edu>
2020-07-07 18:55:42 +0200

    commit final

--- exo_18.rb
+++ exo_18.rb
@@ -1,8 +1,7 @@
 mails = []
-i = 0
+i = 1
 50.times do
     mails.append("jean.dupont."+i.to_s+"@email.fr")
     i = i + 1
     end 
-
 puts mails
--- exo_19.rb
+++ exo_19.rb
@@ -1,12 +1,7 @@
 mails = []
-i = 0
+i = 1
 50.times do
     mails.append("jean.dupont."+i.to_s+"@email.fr")
     i = i + 1
     end 
-
-if i % 2 == 0
-    puts mails
-end 
-
-#mails.sort {val.to_i % 2 <=> 0}
+puts mails
